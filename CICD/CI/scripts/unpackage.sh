04f6f440632a2c93a2f3dd5a13ff430fe98f79bb
Luis Marques <luis.fls.marques@gmail.com>
2020-11-25 13:50:22 +0000

    more pipe changes

--- CICD/CI/scripts/unpackage.sh
+++ CICD/CI/scripts/unpackage.sh
@@ -2,3 +2,6 @@
 
 tar -xzvf ./artifact-repository/app.tar.gz -C ./extracted-package
 find .
+echo 'managed to do this change to the pipeline without needing to update it via fly. just updated source code'
+echo 'after source code is updated this script is included in it'
+echo 'which means pipeline auto-updates without fly VERY COOL!'
